fa8ed4b1f5aba1183bb5647c81facd32208c83df
guilhermegaijutis <gaijuter@gmail.com>
2024-05-04 11:27:42 -0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    println("Hello World!")
+}
+
+/* Repositorio = Pasta
+   Brancha = Copia do codigo (Versão)
+
+   COMANDOS
+
+   - git version
+   - git config --global user.name
+   - git config --global user.email
+   - git commit -m "comentario" (Seleciona)
+   - git push (Envio para o Github)
+   - git pull (Recebe do Github)
+   - git add .  (Seleciona todos os arquivos)
+   - Git Cheat Sheet (Comandos)
+
+
+
+
+
+ */
